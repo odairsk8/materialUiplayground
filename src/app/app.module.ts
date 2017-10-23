f4251a323e8167a3b273fcece22314341797f2e2
Odair Ferreira <odairsk8@hotmail.com>
2017-10-23 11:11:22 +1000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+
+import {MatToolbarModule, MatButtonModule, MatCheckboxModule, MatMenuModule, MatDialogModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule, MatCheckboxModule, MatMenuModule, MatDialogModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
